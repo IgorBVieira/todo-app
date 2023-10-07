c9bc2826606d53f100957e75645db0af2013891f
IgorBVieira <igorvieira.org@gmail.com>
2023-10-07 11:10:24 -0300

    create form

--- src/App.js
+++ src/App.js
@@ -11,17 +11,60 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    }
+
+    console.log(todo);
+    
+    setTime("");
+    setTitle("");
+  };
+
   return (
     <div className="App">
       <div className="todo-header">
         <h1>Todo List</h1>
       </div>
       <div className="form-todo">
-        <p>Formulário</p>
+        <p>Insira sua próxima tarefa:</p>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input
+            type="text"
+            name="title"
+            placeholder="Titulo da tarefa"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title || ''}
+            required
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Duração: </label>
+            <input
+            type="text"
+            name="time"
+            placeholder="Tempo estimado (em horas)"
+            onChange={(e) => setTime(e.target.value)}
+            value={time || ''}
+            required
+            />
+          </div>
+          <input type="submit" value={"Criar Tarefa"} />
+        </form>
       </div>
       <div className="list-todo">
         <h2>Lista de Tarefas:</h2>
-        {todos.length === 0 && <div className="empty-todo">Nenhuma tarefa encontrada</div>}
+        {todos.length === 0 && (
+          <div className="empty-todo">Nenhuma tarefa encontrada</div>
+        )}
       </div>
     </div>
   );
